924e3a9f677d93faa652d54abe645310676eb582
Sam Reed <sam@reedyboy.net>
2015-04-17 18:05:28 -0400

    Update weather.php
    Pireps

--- debug/weather.php
+++ debug/weather.php
@@ -7,29 +7,49 @@ if ( $argc === 1 ) {
         die( "php $argv[0] <Departure Airport> <Destination Airport> [<Alternate>]\n" );
 }
 //var_dump( $argv ); die ();
-echo "Metar/Atis Departure Airport: " . doRequest( "metar", $argv[1] ) . PHP_EOL;
-echo "TAF at Destination: " . doRequest( "taf", $argv[2] ) . PHP_EOL;
+echo "Metar/Atis Departure Airport: " . doMETAR( $argv[1] ) . PHP_EOL;
+echo "TAF at Destination: " . doTAF( $argv[2] ) . PHP_EOL;
 if ( isset( $argv[3] ) ) {
-        echo "TAF at Alternate: " . doRequest( "taf", $argv[3] ) . PHP_EOL;
+        echo "TAF at Alternate: " . doTAF( $argv[3] ) . PHP_EOL;
 }
 
-echo "Current Area Analysis/Synopsis: ";
-echo "Prognosis for next few hours ";
-echo "Sigmets, Airmets and Convective Sigmets:";
-echo "Pireps";
-echo "Winds, Temps & Freezing Level:";
-echo "Notams and TFR:";
+//echo "Current Area Analysis/Synopsis: " . PHP_EOL;
+//echo "Prognosis for next few hours: " . PHP_EOL;
+//echo "Sigmets, Airmets and Convective Sigmets: " . PHP_EOL;
+echo "Pireps: " . PHP_EOL;
+echo doPIREP( $argv[1], $argv[2] );
+//echo "Winds, Temps & Freezing Level: " . PHP_EOL;
+//echo "Notams and TFR: " . PHP_EOL;
 
+function doTAF( $station ) {
+        return doSimpleRequest( "taf", $station );
+}
 
-function doRequest( $type, $station, $additional = '' ) {
-// Use mostrecent
-        $url = "https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource={$type}s&requestType=retrieve&format=xml&stationString=$station&hoursBeforeNow=1";
-        if ( $additional ) {
-                $url .= "&" . $additional
+function doMETAR( $station ) {
+        return doSimpleRequest( "metar", $station );
+}
+
+function doPIREP( $from, $to ) {
+        $items = doRequest( "pirep", "flightPath=57.5;$from;$to" );
+        preg_match_all( "/\<raw_text\>(.*?)\<\/raw_text\>/im", $items, $matches );
+        $matches = array();
+        $res = '';
+        foreach ( $matches as $pirep ) {
+                $res .= $pirep[1] . PHP_EOL;
         }
-        $item = file_get_contents( $url );
+        return $res;
+}
 
+
+function doSimpleRequest( $type, $station ) {
+        $item = doRequest( $type, "stationString=$station" );
         $matches = array();
         preg_match( "/\<raw_text\>(.*?)\<\/raw_text\>/im", $item, $matches );
         return $matches[1];
+}
+
+function doRequest( $type, $additional = '' ) {
+        // Use mostrecent
+        $url = "https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource={$type}s&requestType=retrieve&format=xml&hoursBeforeNow=1&$additional";
+        return file_get_contents( $url );
 }
